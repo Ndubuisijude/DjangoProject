94bc003ff45153677d71732e01cbbb92c6def06a
jude <ndubuisijude46@gmail.com>
2025-10-09 10:10:03 +0100

    myproject

--- myproject/student/urls.py
+++ myproject/student/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('student/', views.student_view, name='student_view'),
+    path('come_inside/', views.come_inside, name='come_inside'),
+    path('name', views.say_Name, name='say_name'),
+    path('index/', views.index, name='student'),
+    path('jude/', views.jude, name='jude'),
+    path('family/', views.family, name='family'),
 ]
--- myproject/student/views.py
+++ myproject/student/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 
 #return a simple http response
 def student_view(request):
     return HttpResponse("Hello, this is the student view.")
+
+def come_inside(request):
+    return HttpResponse("welcome inside")
+
+def say_Name(request):
+    return HttpResponse("My name is Jude")
+
+def index(request):
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render())
+
+def jude(request):
+    template = loader.get_template("jude.html")
+    return HttpResponse(template.render())
+
+def family(request):
+    template = loader.get_template("family.html")
+    return HttpResponse(template.render())
